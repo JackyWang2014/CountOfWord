41f4eadf2f867aafdbe7beb2def38d1e30a1684e
WangQitai <wangqitai@WangQitaideMacBook-Pro.local>
2016-06-09 11:53:19 +0800

    First initial count of word

--- CountOfWord/main.c
+++ CountOfWord/main.c
@@ -9,7 +9,23 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char a[100] = "I am a boy. who?   are you? haha ! i am good!";;
+    //        gets(a);
+    //        scanf("%s",a);
+    int cnt = 0;
+    int word = 0;
+    
+    char c;
+    //            printf("%s",a);
+    //            printf("%d",strlen(a));
+    for (int i = 0; (c = a[i] )!= '\0'; i++) {
+        if (c == ' ' || c=='!' || c=='?' || c=='?') {
+            word = 0;
+        } else if(word == 0){
+            word = 1;
+            cnt++;
+        }
+    }    
+    printf("%d\n",cnt);
     return 0;
 }
